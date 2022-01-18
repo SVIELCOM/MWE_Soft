b544511f564a77be6b20783ab3a2bbc6949d3d34
Vlad <svielcom@gmail.com>
2022-01-18 11:53:00 +0700

    ADCandTIMconfig foldered

---
+++ Weight_calculation/Inc/path_switch.h
@@ -0,0 +1,14 @@
+/*
+ * path_switch.h
+ *
+ *  Created on: 18 янв. 2022 г.
+ *      Author: cableman
+ */
+
+#ifndef PATH_SWITCH_H_
+#define PATH_SWITCH_H_
+
+#define PATH_SWITH_PIN		15
+#define PATH_SWITH_
+
+#endif /* PATH_SWITCH_H_ */
---
+++ Weight_calculation/Src/path_switch.c
@@ -0,0 +1,8 @@
+/*
+ * path_switch.c
+ *
+ *  Created on: 18 янв. 2022 г.
+ *      Author: cableman
+ */
+
+
